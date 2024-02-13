077c4542694f3081dd6e3195dce0d3278e2d27de
Abigail S <abiskepple@gmail.com>
2024-02-13 16:45:24 -0400

    Files added

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+ <h2>Abigail Skepple</h2>
+ 
+ <p>I am pursuing a Bachelors Degree in Information Technology, at the UWI Five Islands Campus. I am currently in my third year. 
+	I aspire to become a Software Engineer, Web Developer or Database Administrator in the future.</p>
+
+@endsection 
